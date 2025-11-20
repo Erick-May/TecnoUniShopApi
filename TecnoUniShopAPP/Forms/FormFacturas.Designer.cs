93e772a14325b4adda6491103c94973b675e096e
JASARETH ENMANUEL MENDOZA FLORES <jasareth.mendoza641@std.uni.edu.ni>
2025-11-19 20:53:21 -0600

    7ma interfaz

--- TecnoUniShopAPP/Forms/FormFacturas.Designer.cs
+++ TecnoUniShopAPP/Forms/FormFacturas.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormFacturas));
             label1 = new Label();
             label2 = new Label();
             dgvFacturas = new DataGridView();
@@ -40,66 +41,83 @@
             // label1
             // 
             label1.AutoSize = true;
-            label1.Font = new Font("Sitka Text", 16F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
-            label1.Location = new Point(306, 9);
+            label1.Font = new Font("Segoe UI", 22.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(245, 7);
+            label1.Margin = new Padding(2, 0, 2, 0);
             label1.Name = "label1";
-            label1.Size = new Size(361, 47);
+            label1.Size = new Size(389, 50);
             label1.TabIndex = 14;
             label1.Text = "REPORTE DE VENTAS";
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Font = new Font("Sitka Text", 16F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
-            label2.Location = new Point(306, 304);
+            label2.Font = new Font("Segoe UI", 22.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.Location = new Point(257, 254);
+            label2.Margin = new Padding(2, 0, 2, 0);
             label2.Name = "label2";
-            label2.Size = new Size(343, 47);
+            label2.Size = new Size(367, 50);
             label2.TabIndex = 15;
             label2.Text = "DETALLE FACTURAS";
             // 
             // dgvFacturas
             // 
+            dgvFacturas.BackgroundColor = SystemColors.ActiveCaption;
             dgvFacturas.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dgvFacturas.Location = new Point(12, 59);
+            dgvFacturas.Location = new Point(16, 59);
+            dgvFacturas.Margin = new Padding(2, 2, 2, 2);
             dgvFacturas.Name = "dgvFacturas";
             dgvFacturas.RowHeadersWidth = 62;
-            dgvFacturas.Size = new Size(1062, 225);
+            dgvFacturas.Size = new Size(863, 193);
             dgvFacturas.TabIndex = 16;
             dgvFacturas.SelectionChanged += dgvFacturas_SelectionChanged;
             // 
             // dgvDetalles
             // 
+            dgvDetalles.BackgroundColor = SystemColors.ActiveCaption;
             dgvDetalles.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dgvDetalles.Location = new Point(12, 369);
+            dgvDetalles.Location = new Point(16, 306);
+            dgvDetalles.Margin = new Padding(2, 2, 2, 2);
             dgvDetalles.Name = "dgvDetalles";
             dgvDetalles.RowHeadersWidth = 62;
-            dgvDetalles.Size = new Size(1062, 225);
+            dgvDetalles.Size = new Size(863, 193);
             dgvDetalles.TabIndex = 17;
             // 
             // btnVolver
             // 
-            btnVolver.Location = new Point(12, 633);
+            btnVolver.BackColor = Color.MidnightBlue;
+            btnVolver.Cursor = Cursors.Hand;
+            btnVolver.FlatAppearance.MouseOverBackColor = Color.Silver;
+            btnVolver.FlatStyle = FlatStyle.Flat;
+            btnVolver.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnVolver.ForeColor = SystemColors.ControlLightLight;
+            btnVolver.Location = new Point(354, 508);
+            btnVolver.Margin = new Padding(2, 2, 2, 2);
             btnVolver.Name = "btnVolver";
-            btnVolver.Size = new Size(199, 44);
+            btnVolver.Size = new Size(177, 60);
             btnVolver.TabIndex = 18;
             btnVolver.Text = "Volver";
-            btnVolver.UseVisualStyleBackColor = true;
+            btnVolver.UseVisualStyleBackColor = false;
             btnVolver.Click += btnVolver_Click;
             // 
             // FormFacturas
             // 
-            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1096, 689);
+            BackColor = SystemColors.ControlLightLight;
+            ClientSize = new Size(903, 579);
             Controls.Add(btnVolver);
             Controls.Add(dgvDetalles);
             Controls.Add(dgvFacturas);
             Controls.Add(label2);
             Controls.Add(label1);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Margin = new Padding(2, 2, 2, 2);
             MaximizeBox = false;
             MinimizeBox = false;
             Name = "FormFacturas";
-            Text = "FormFacturas";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Facturas";
             Load += FormFacturas_Load;
             ((System.ComponentModel.ISupportInitialize)dgvFacturas).EndInit();
             ((System.ComponentModel.ISupportInitialize)dgvDetalles).EndInit();
