2f6c188bf0c945931324d026ff4f08b34e547102
JASARETH ENMANUEL MENDOZA FLORES <jasareth.mendoza641@std.uni.edu.ni>
2025-11-19 20:47:39 -0600

    6ta interfaz

--- TecnoUniShopAPP/Forms/FormCarrito.Designer.cs
+++ TecnoUniShopAPP/Forms/FormCarrito.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormCarrito));
             dgvCarrito = new DataGridView();
             label1 = new Label();
             btnComprar = new Button();
@@ -39,77 +40,101 @@
             // 
             // dgvCarrito
             // 
+            dgvCarrito.BackgroundColor = SystemColors.ActiveCaption;
             dgvCarrito.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dgvCarrito.Location = new Point(28, 70);
+            dgvCarrito.Location = new Point(23, 59);
+            dgvCarrito.Margin = new Padding(2, 2, 2, 2);
             dgvCarrito.Name = "dgvCarrito";
             dgvCarrito.RowHeadersWidth = 62;
-            dgvCarrito.Size = new Size(935, 315);
+            dgvCarrito.Size = new Size(788, 252);
             dgvCarrito.TabIndex = 0;
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Font = new Font("Sitka Text", 16F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
-            label1.Location = new Point(301, 9);
+            label1.Font = new Font("Segoe UI", 22.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(206, 7);
+            label1.Margin = new Padding(2, 0, 2, 0);
             label1.Name = "label1";
-            label1.Size = new Size(385, 47);
+            label1.Size = new Size(427, 50);
             label1.TabIndex = 12;
             label1.Text = "CARRITO DE COMPRAS\r\n";
             // 
             // btnComprar
             // 
-            btnComprar.Location = new Point(28, 505);
+            btnComprar.BackColor = Color.MidnightBlue;
+            btnComprar.Cursor = Cursors.Hand;
+            btnComprar.FlatAppearance.MouseOverBackColor = Color.FromArgb(0, 192, 0);
+            btnComprar.FlatStyle = FlatStyle.Flat;
+            btnComprar.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnComprar.ForeColor = SystemColors.ControlLightLight;
+            btnComprar.Location = new Point(206, 395);
+            btnComprar.Margin = new Padding(2, 2, 2, 2);
             btnComprar.Name = "btnComprar";
-            btnComprar.Size = new Size(189, 54);
+            btnComprar.Size = new Size(177, 60);
             btnComprar.TabIndex = 14;
             btnComprar.Text = "COMPRAR";
-            btnComprar.UseVisualStyleBackColor = true;
+            btnComprar.UseVisualStyleBackColor = false;
             btnComprar.Click += btnComprar_Click;
             // 
             // btnVolver
             // 
-            btnVolver.Location = new Point(257, 505);
+            btnVolver.BackColor = Color.MidnightBlue;
+            btnVolver.Cursor = Cursors.Hand;
+            btnVolver.FlatAppearance.MouseOverBackColor = Color.Silver;
+            btnVolver.FlatStyle = FlatStyle.Flat;
+            btnVolver.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnVolver.ForeColor = SystemColors.ControlLightLight;
+            btnVolver.Location = new Point(456, 395);
+            btnVolver.Margin = new Padding(2, 2, 2, 2);
             btnVolver.Name = "btnVolver";
-            btnVolver.Size = new Size(189, 54);
+            btnVolver.Size = new Size(177, 60);
             btnVolver.TabIndex = 15;
             btnVolver.Text = "VOLVER";
-            btnVolver.UseVisualStyleBackColor = true;
+            btnVolver.UseVisualStyleBackColor = false;
             btnVolver.Click += btnVolver_Click;
             // 
             // lblTotal
             // 
             lblTotal.AutoSize = true;
-            lblTotal.Font = new Font("Sitka Text", 16F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
-            lblTotal.Location = new Point(28, 401);
+            lblTotal.Font = new Font("Segoe UI Semibold", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblTotal.Location = new Point(206, 322);
+            lblTotal.Margin = new Padding(2, 0, 2, 0);
             lblTotal.Name = "lblTotal";
-            lblTotal.Size = new Size(251, 47);
+            lblTotal.Size = new Size(180, 38);
             lblTotal.TabIndex = 16;
-            lblTotal.Text = " Total: C$ 0.00";
+            lblTotal.Text = " Total: $ 0.00";
             // 
             // cmbMetodoPago
             // 
+            cmbMetodoPago.BackColor = SystemColors.GradientActiveCaption;
             cmbMetodoPago.DropDownStyle = ComboBoxStyle.DropDownList;
             cmbMetodoPago.FormattingEnabled = true;
-            cmbMetodoPago.Location = new Point(649, 401);
+            cmbMetodoPago.Location = new Point(418, 332);
+            cmbMetodoPago.Margin = new Padding(2, 2, 2, 2);
             cmbMetodoPago.Name = "cmbMetodoPago";
-            cmbMetodoPago.Size = new Size(314, 33);
+            cmbMetodoPago.Size = new Size(252, 28);
             cmbMetodoPago.TabIndex = 17;
             // 
             // FormCarrito
             // 
-            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1005, 571);
+            BackColor = SystemColors.ControlLightLight;
+            ClientSize = new Size(840, 481);
             Controls.Add(cmbMetodoPago);
             Controls.Add(lblTotal);
             Controls.Add(btnVolver);
             Controls.Add(btnComprar);
             Controls.Add(label1);
             Controls.Add(dgvCarrito);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Margin = new Padding(2, 2, 2, 2);
             MaximizeBox = false;
             MinimizeBox = false;
             Name = "FormCarrito";
-            Text = "FormCarrito";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Carrito";
             Load += FormCarrito_Load;
             ((System.ComponentModel.ISupportInitialize)dgvCarrito).EndInit();
             ResumeLayout(false);
