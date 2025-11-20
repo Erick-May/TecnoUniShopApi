c7a243c668344bf17e8b0cadc20e877e07cffafb
JASARETH ENMANUEL MENDOZA FLORES <jasareth.mendoza641@std.uni.edu.ni>
2025-11-19 18:15:30 -0600

    First Interfaz

--- TecnoUniShopAPP/Forms/FormBienvenida.Designer.cs
+++ TecnoUniShopAPP/Forms/FormBienvenida.Designer.cs
@@ -28,52 +28,91 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormBienvenida));
             btnEntrar = new Button();
             label1 = new Label();
             label2 = new Label();
+            panel1 = new Panel();
+            pictureBox1 = new PictureBox();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
             // btnEntrar
             // 
-            btnEntrar.Font = new Font("Segoe UI", 12F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
-            btnEntrar.Location = new Point(388, 208);
+            btnEntrar.BackColor = Color.MidnightBlue;
+            btnEntrar.Cursor = Cursors.Hand;
+            btnEntrar.FlatAppearance.MouseOverBackColor = Color.FromArgb(0, 192, 0);
+            btnEntrar.FlatStyle = FlatStyle.Flat;
+            btnEntrar.Font = new Font("Segoe UI Semibold", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnEntrar.ForeColor = SystemColors.ControlLightLight;
+            btnEntrar.Location = new Point(634, 388);
+            btnEntrar.Margin = new Padding(2, 2, 2, 2);
             btnEntrar.Name = "btnEntrar";
-            btnEntrar.Size = new Size(172, 59);
+            btnEntrar.Size = new Size(172, 54);
             btnEntrar.TabIndex = 0;
             btnEntrar.Text = "Entrar";
-            btnEntrar.UseVisualStyleBackColor = true;
+            btnEntrar.UseVisualStyleBackColor = false;
             btnEntrar.Click += btnEntrar_Click;
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Font = new Font("Stencil", 16F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label1.Location = new Point(86, 40);
+            label1.Font = new Font("Segoe UI Semibold", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(471, 252);
+            label1.Margin = new Padding(2, 0, 2, 0);
             label1.Name = "label1";
-            label1.Size = new Size(771, 38);
+            label1.Size = new Size(505, 41);
             label1.TabIndex = 1;
-            label1.Text = "¡BIENVENIDO A TU TIENDA EN LINEA FAVORITA!";
+            label1.Text = "Tu futuro tecnológico empieza aquí";
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Font = new Font("Stencil", 16F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label2.Location = new Point(322, 113);
+            label2.Font = new Font("Segoe UI", 25.8000011F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.Location = new Point(526, 114);
+            label2.Margin = new Padding(2, 0, 2, 0);
             label2.Name = "label2";
-            label2.Size = new Size(283, 38);
+            label2.Size = new Size(393, 60);
             label2.TabIndex = 2;
             label2.Text = "TECNO UNI SHOP\r\n";
             // 
+            // panel1
+            // 
+            panel1.BackColor = Color.MidnightBlue;
+            panel1.Controls.Add(pictureBox1);
+            panel1.Dock = DockStyle.Left;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(466, 580);
+            panel1.TabIndex = 3;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(0, 60);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(466, 455);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
             // FormBienvenida
             // 
-            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(951, 450);
+            BackColor = SystemColors.ControlLightLight;
+            ClientSize = new Size(986, 580);
+            Controls.Add(panel1);
             Controls.Add(label2);
             Controls.Add(label1);
             Controls.Add(btnEntrar);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Margin = new Padding(2, 2, 2, 2);
             Name = "FormBienvenida";
-            Text = "Form1";
+            Text = "TECNO UNI SHOP";
+            panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -83,5 +122,7 @@
         private Button btnEntrar;
         private Label label1;
         private Label label2;
+        private Panel panel1;
+        private PictureBox pictureBox1;
     }
 }
